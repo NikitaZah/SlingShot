5cbcd420fae8d698e2db9986bbb9ce36de19b224
Your Name <you@example.com>
2021-12-04 22:15:00 +0300

    Added BaseSymbol and BaseTradeData classes

--- symbol.py
+++ symbol.py
@@ -2,39 +2,43 @@ from decimal import *
 from datetime import datetime, timedelta
 
 
-class TradeData:
-    def __init__(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_price, stop_loss_order,
-                 stop_loss_type: str):
+class BaseTradeData:
+    def __init__(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_order: int = None,
+                 stop_loss_price: Decimal = None, stop_loss_type: str = None, take_profit_order: int = None,
+                 take_profit_price: Decimal = None):
         self.start_date: datetime = datetime.now()
+        self.side = side
         self.start_price = start_price
         self.original_quantity = orig_qty
-        self.side = side
+        self.current_quantity = orig_qty
+        self.current_price = start_price
+
+        self.stop_loss_price = stop_loss_price
+        self.stop_loss = stop_loss_order
+        self.stop_loss_type = stop_loss_type
+        self.rake_profit_price = take_profit_price
+        self.take_profit = take_profit_order
+        self.result = Decimal(0)     # redefine depending on market/limit order and current commission fee
 
         if self.side == 'BUY':
             self.close_side = 'SELL'
         else:
             self.close_side = 'BUY'
 
-        self.addons = 1
-        self.fixes = 0
+    def new_order(self, side: str, qty: Decimal, price: Decimal, price_step: Decimal):
+        if side == self.side:
+            return self.addon(qty, price, price_step)
+        if side == self.close_side:
+            return self.fix(qty, price)
 
-        self.current_quantity = orig_qty
-        self.current_price = start_price
+    def fix(self, qty: Decimal, price: Decimal):
+        self.current_quantity -= qty
+        self.result += (price - self.current_price) * qty if self.side == 'BUY' else -(price - self.current_price) * qty
+        #   add commission fee to current result
 
-        self.last_addon_time = self.start_date
-        self.last_fix_time = None
-        self.last_fix_price = None
-
-        self.stop_loss_price = stop_loss_price
-        self.stop_loss = stop_loss_order
-        self.stop_loss_type = stop_loss_type
-
-        self.stop_loss_percent = abs((self.stop_loss_price / self.start_price - 1).quantize(Decimal('0.01')))
-
-        self.result = - self.original_quantity * self.start_price * Decimal(0.0004)     # binance fee for maker
+        return {'closed': self.current_quantity == Decimal(0)}
 
     def addon(self, qty: Decimal, price: Decimal, price_step: Decimal):
-        self.original_quantity = self.current_quantity + qty
         self.current_price = (self.current_price * self.current_quantity + price * qty) / (self.current_quantity + qty)
 
         if self.side == 'BUY':
@@ -42,35 +46,47 @@ class TradeData:
         else:
             self.current_price = Decimal(self.current_price).quantize(price_step, rounding=ROUND_DOWN)
 
-        self.current_quantity = self.original_quantity
+        self.current_quantity += qty
+        # add commission fee to result
+
+        return {'price': self.current_price, 'qty': self.current_quantity}
+
+    def statistics(self):
+        return {
+            'start_date': self.start_date,
+            'side': self.side,
+            'max_qty': self.original_quantity,
+            'current_qty': self.current_quantity,
+            'result': self.result
+        }
+
+
+class TradeData(BaseTradeData):
+    def __init__(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_price, stop_loss_order,
+                 stop_loss_type: str):
+        super(TradeData, self).__init__(side, orig_qty, start_price, stop_loss_price, stop_loss_order, stop_loss_type)
+        self.addons = 1
+        self.fixes = 0
+        self.last_addon_time = self.start_date
+        self.last_fix_time = None
+        self.last_fix_price = None
+
+        self.stop_loss_percent = abs((self.stop_loss_price / self.start_price - 1).quantize(Decimal('0.01')))
+        self.result = - self.original_quantity * self.start_price * Decimal(0.0004)     # binance fee for maker
+
+    def addon(self, qty: Decimal, price: Decimal, price_step: Decimal):
         self.last_addon_time = datetime.now()
         self.addons += 1
         self.result -= qty * price * Decimal(0.0004)
-
-        response = {'stop_loss': None}
-        if self.stop_loss_type == 'STOP_MARKET':    # price where  should be moved stop loss market
-            response['stop_loss'] = self.current_price
-        return response
+        self.original_quantity = self.current_quantity + qty
+        return super(TradeData, self).addon(qty, price, price_step)
 
     def fix(self, qty: Decimal, price: Decimal):
-        self.current_quantity -= qty
         self.last_fix_time = datetime.now()
         self.fixes += 1
         self.last_fix_price = price
-        self.result += (price - self.current_price) * qty if self.side == 'BUY' else -(price - self.current_price) * qty
         self.result -= qty * price * Decimal(0.0004)
-
-        response = {'result': self.result, 'stop_loss': None, 'closed': False}
-        if self.current_quantity > 0:
-            if self.stop_loss_type == 'STOP_MARKET':    # price where should be moved stop loss market
-                if self.side == 'BUY':
-                    if price > self.current_price * (1 + self.stop_loss_percent):
-                        response['stop_loss'] = self.current_price
-                else:
-                    if price < self.current_price * (1 - self.stop_loss_percent):
-                        response['stop_loss'] = self.current_price
-        else:
-            response['closed'] = True
+        return super(TradeData, self).fix(qty, price)
 
     def fix_allowed(self):
         return self.last_addon_time < datetime.now() - timedelta(hours=3)
@@ -81,28 +97,62 @@ class TradeData:
         else:
             return self.current_price * Decimal('0.92') > price
 
+    def statistics(self):
+        return {
+            'start_date': self.start_date,
+            'close_date': self.last_fix_time,
+            'side': self.side,
+            'max_qty': self.original_quantity,
+            'current_qty': self.current_quantity,
+            'total_addons': self.addons,
+            'total_fixes': self.fixes,
+            'result': self.result
+        }
 
-class Symbol:
+
+class BaseSymbol:
     def __init__(self, symbol: str, price_step: Decimal, lot_step: Decimal, min_qty: Decimal):
         self.symbol = symbol
         self.price_step = price_step
         self.lot_step = lot_step
         self.min_qty = min_qty
 
-        self.trend = 0
-        self.day_volume = 0
-
         self.in_trade = False
         self.trade_data = None
 
-    def start_trade(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_price, stop_loss_order,
-                    stop_loss_kind):
+    # override this method using TradeData(BaseTradeData) object
+    def start_trade(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_price: Decimal = None,
+                    stop_loss_order: int = None, stop_loss_type: str = None, take_profit_order: int = None,
+                    take_profit_price: Decimal = None):
         self.in_trade = True
-        self.trade_data = TradeData(side, orig_qty, start_price, stop_loss_price, stop_loss_order, stop_loss_kind)
+        self.trade_data = BaseTradeData(side, orig_qty, start_price, stop_loss_price, stop_loss_order, stop_loss_type,
+                                        take_profit_order, take_profit_price)
 
-    def close_position(self):
+    def stop_trade(self):
+        result = self.trade_data.statistics()
         self.in_trade = False
         self.trade_data = None
+        return result
+
+    def update_trade_data(self, order: dict = None, new_sl: dict = None, new_tp: dict = None):
+        if not self.in_trade:
+            return
+        if order:
+            self.trade_data.new_order(order['side'], order['qty'], order['price'], self.price_step)
+        if new_sl:
+            self.trade_data.stop_loss = new_sl['order_id']
+            self.trade_data.stop_loss_price = new_sl['price']
+        if new_tp:
+            self.trade_data.take_profit = new_tp['order_id']
+            self.trade_data.take_profit_price = new_tp['price']
+
+
+class Symbol(BaseSymbol):
+    def start_trade(self, side: str, orig_qty: Decimal, start_price: Decimal, stop_loss_price: Decimal = None,
+                    stop_loss_order: int = None, stop_loss_type: str = None, take_profit_order: int = None,
+                    take_profit_price: Decimal = None):
+        self.in_trade = True
+        self.trade_data = TradeData(side, orig_qty, start_price, stop_loss_price, stop_loss_order, stop_loss_type)
 
     def fix_qty(self, parts: int):
         if not self.in_trade:
@@ -123,19 +173,6 @@ class Symbol:
             print(f'{self.symbol} is not in trade. Attempt to get close quantity failed')
             return None
         return self.trade_data.current_quantity
-
-    def update(self, side: str, qty: Decimal, price: Decimal):
-        if not self.in_trade:
-            print(f'{self.symbol} is not in trade. Attempt to update data failed')
-            return None
-        if side == self.trade_data.side:
-            return self.trade_data.addon(qty, price, self.price_step)
-        else:
-            return self.trade_data.fix(qty, price)
-
-    def update_stop_loss(self, new_sl: int, new_sl_price: Decimal):
-        self.trade_data.stop_loss = new_sl
-        self.trade_data.stop_loss_price = new_sl_price
 
     def quantity(self, price: Decimal, quote_qty: Decimal):
         qty = (quote_qty / price).quantize(self.lot_step, rounding=ROUND_DOWN)
